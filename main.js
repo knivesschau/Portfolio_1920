48ebd1d0809ff6045ecbc7886cf0171c44ca786f
Catrina Fuentes <cat.fuentes93@gmail.com>
2020-01-13 14:42:25 -0800

    fixed linking

--- main.js
+++ main.js
@@ -4,35 +4,35 @@
 function hideHTML() {
     console.log("hideHTML ran");
 
-    $(".about-me, .portfolio-page, .current-skillset, .contact-info").hide();
+    $(".about-page, .portfolio-page, .current-skillset, .contact-info").hide();
 }
 
 //this function shows other HTML sections when clicked on so that the user can navigate through the portfolio. 
 function portNavigation() {
     console.log("portNavigation ran");
 
-    $("#about").on("click", function(event) {
+    $("#about-page").on("click", function(event) {
         console.log("click 1 ran!");
-        $(".about-me").show();
+        $(".about-page").show();
         $(".portfolio-landing, .portfolio-page, .current-skillset, .contact-info").hide();
     });
 
     $("#portfolio").on("click", function(event) {
         console.log("click 2 ran!");
         $(".portfolio-page").show();
-        $(".portfolio-landing, .about-me, .current-skillset, .contact-info").hide();
+        $(".portfolio-landing, .about-page, .current-skillset, .contact-info").hide();
     });
 
     $("#skills").on("click", function(event){
         console.log("click 3 ran!");
          $(".current-skillset").show();
-         $(".portfolio-landing, .about-me, .portfolio-page, .contact-info").hide();
+         $(".portfolio-landing, .about-page, .portfolio-page, .contact-info").hide();
     });
 
-    $("#contact").on("click", function(event) {
+    $("#contact-page").on("click", function(event) {
         console.log("click 4 ran!");
         $(".contact-info").show();
-        $(".portfolio-landing, .about-me, .portfolio-page, .current-skillset").hide();
+        $(".portfolio-landing, .about-page, .portfolio-page, .current-skillset").hide();
     });
 }
 
