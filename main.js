84ecdb3fa0339461747aa0dfa45a44e44e17e7ee
Catrina Fuentes <cat.fuentes93@gmail.com>
2020-01-29 17:21:56 -0800

    first round of additional revisions

--- main.js
+++ main.js
@@ -1,33 +1,39 @@
 'use strict';
 
-//this function hides all other HTML sections so the user lands on the tagline, footer, and nav links.
-function hideHTML() {
-    $(".about-page, .portfolio-page, .current-skillset, .contact-info, .portfolio-landing").hide();
+//this function renders the default portfolio view in mobile AND desktop. 
+function portfolioRender() {
+    loadIn();
+    portNavigation();
+    resetPortfolio();
+}
+
+//this function handles the animation of the landing page.
+function loadIn() {
     $("document").ready(function() {
-        $(".portfolio-landing").fadeIn(2000);
+        $(".portfolio-landing").fadeIn(400);
     });
 }
 
 //this function shows other HTML sections when clicked on so that the user can navigate through the portfolio. 
 function portNavigation() {
     $("#about-page").on("click", function(event) {
-        $(".about-page").fadeIn(600);
-        $(".portfolio-landing, .portfolio-page, .current-skillset, .contact-info").hide();
+        $(".about-page").fadeIn(400)
+        $(".portfolio-landing, .portfolio-page, .current-skillset, .contact-info").hide(); 
     });
     
 
     $("#portfolio").on("click", function(event) {
-        $(".portfolio-page").fadeIn(600);
+        $(".portfolio-page").fadeIn(400);
         $(".portfolio-landing, .about-page, .current-skillset, .contact-info").hide();
     });
 
     $("#skills").on("click", function(event){
-         $(".current-skillset").fadeIn(600);
-         $(".portfolio-landing, .about-page, .portfolio-page, .contact-info").hide();
+         $(".current-skillset").fadeIn(400);
+         $(".portfolio-landing, .about-page, .portfolio-page, .contact-info").hide(); 
     });
 
     $("#contact-page").on("click", function(event) {
-        $(".contact-info").fadeIn(600);
+        $(".contact-info").fadeIn(400);
         $(".portfolio-landing, .about-page, .portfolio-page, .current-skillset").hide();
     });
 }
@@ -35,15 +41,9 @@ function portNavigation() {
 //this function resets the portfolio every time the user clicks "Home".
 function resetPortfolio() {
     $("#home").on("click", function(event) {
-        hideHTML();
+        loadIn();
+        $(".about-page, .portfolio-page, .current-skillset, .contact-info").hide();
     });
-}
-
-//this function renders the default portfolio view in mobile AND desktop. 
-function portfolioRender() {
-    hideHTML();
-    portNavigation();
-    resetPortfolio();
-}
+ }
 
 $(portfolioRender);
